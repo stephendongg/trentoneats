f565fa3e680a0eeb4dd70c295db52b71294d5189
pozuah <74800408+pozuah@users.noreply.github.com>
2022-03-18 20:47:39 -0400

    Add files via upload

--- database/create_table.py
+++ database/create_table.py
@@ -4,15 +4,17 @@
 # Group: Trenton Eats Local
 # Author: Piers Ozuah
 
+
+from sys import argv, stderr, exit
+from psycopg2 import connect
 import psycopg2
-from config import config
 
 
 def create_tables():
     """ create tables in the PostgreSQL database"""
     commands = (
         """
-        CREATE TABLE [IF NOT EXISTS] restaurants (
+        CREATE TABLE IF NOT EXISTS restaurants (
             restaurant_id VARCHAR(20) PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             address VARCHAR(255) NOT NULL,
@@ -26,7 +28,7 @@ def create_tables():
         )
         """,
         """ 
-        CREATE TABLE [IF NOT EXISTS] customers (
+        CREATE TABLE IF NOT EXISTS customers (
                 customer_id VARCHAR(20) PRIMARY KEY,
                 name VARCHAR(255),
                 review_count INTEGER NOT NULL,
@@ -36,10 +38,12 @@ def create_tables():
                 )
         """,
         """
-        CREATE TABLE [IF NOT EXISTS] reviews (
+        CREATE TABLE IF NOT EXISTS reviews (
                 review_id INTEGER PRIMARY KEY,
+                customer_id VARCHAR(20),
                 FOREIGN KEY (customer_id) 
                 REFERENCES customers (customer_id),
+                restaurant_id VARCHAR(20),
                 FOREIGN KEY (restaurant_id)
                 REFERENCES restaurants (restaurant_id),
                 date TIMESTAMP NOT NULL,
@@ -52,33 +56,53 @@ def create_tables():
         )
         """,
         """
-        CREATE TABLE [IF NOT EXISTS] categories (
-                category VARCHAR(20) PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS categories (
+                restaurant_id VARCHAR(20) PRIMARY KEY,
+                fast_food BOOLEAN,
+                fine_dining BOOLEAN,
+                casual_dining BOOLEAN,
+                inexpensive BOOLEAN,
+                moderate_price BOOLEAN,
+                pricey BOOLEAN,
+                priciest BOOLEAN,
+                american BOOLEAN,
+                french BOOLEAN,
+                indian BOOLEAN,
                 FOREIGN KEY (restaurant_id)
                 REFERENCES restaurants (restaurant_id)
         )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS usertype (
+            user_id VARCHAR(20) PRIMARY KEY,
+            customer_id VARCHAR(20),
+            FOREIGN KEY (customer_id) 
+            REFERENCES customers (customer_id),
+            restaurant_id VARCHAR(20),
+            FOREIGN KEY (restaurant_id)
+            REFERENCES restaurants (restaurant_id),
+            admin BOOLEAN,
+            restaurant BOOLEAN,
+            customer BOOLEAN
+
+        )
         """)
-    conn = None
 
 # The following code was adapted from POSTGRESQL TUTORIAL
 
     try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # create table one by one
-        for command in commands:
-            cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
+        with connect(
+                host='localhost', port=5432, user='rmd', password='trentoneats333',
+                database='teldatabase') as connection:
+
+            with connection.cursor() as cursor:
+                # create table one by one
+                for command in commands:
+                    cursor.execute(command)
+                # close communication with the PostgreSQL database server
+                cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-
-        conn.close()
 
 
 if __name__ == '__main__':
