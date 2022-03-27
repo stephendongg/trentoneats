27e4e42c2c44b8c01ae81683d7be6a004c23e9c8
stephen <sd20@princeton.edu>
2022-03-26 20:05:25 -0400

    sqltest

--- trentoneats.sql
+++ trentoneats.sql
@@ -25,17 +25,8 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.categories (
-    restaurant_id character varying(20) NOT NULL,
-    fast_food boolean,
-    fine_dining boolean,
-    casual_dining boolean,
-    inexpensive boolean,
-    moderate_price boolean,
-    pricey boolean,
-    priciest boolean,
-    american boolean,
-    french boolean,
-    indian boolean
+    category character varying(20) NOT NULL,
+    restaurant_id character varying(20)
 );
 
 
@@ -98,26 +89,10 @@ CREATE TABLE public.reviews (
 ALTER TABLE public.reviews OWNER TO rmd;
 
 --
--- Name: usertype; Type: TABLE; Schema: public; Owner: rmd
---
-
-CREATE TABLE public.usertype (
-    user_id character varying(20) NOT NULL,
-    customer_id character varying(20),
-    restaurant_id character varying(20),
-    admin boolean,
-    restaurant boolean,
-    customer boolean
-);
-
-
-ALTER TABLE public.usertype OWNER TO rmd;
-
---
 -- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: rmd
 --
 
-COPY public.categories (restaurant_id, fast_food, fine_dining, casual_dining, inexpensive, moderate_price, pricey, priciest, american, french, indian) FROM stdin;
+COPY public.categories (category, restaurant_id) FROM stdin;
 \.
 
 
@@ -134,17 +109,18 @@ COPY public.customers (customer_id, name, review_count, avg_rating, account_type
 --
 
 COPY public.restaurants (restaurant_id, name, address, hours, open_closed, menu, media, tags, review_count, stars) FROM stdin;
+1	Playa Bowls	24 Trenton Avenue, Trenton, NJ	9am-5pm everyday	t	Playa Bowlsabowls.com/trenton	playabowlos.com/media	Acai, Fruit	15	4.2
 2	1911 Smokehouse BBQ	11 W Front St, Trenton, NJ 08608	Monday\t11AM - 3:30PM,\n        Tuesday\t11AM - 3:30PM,\n        Wednesday 11AM - 10:30PM,\n        Thursday 11AM - 10:30PM,\n        Friday 11AM - 10:30PM,\n        Saturday 1 - 10:30PM,\n        Sunday 1 - 10:30PM	f	http://places.singleplatform.com/1911-smoke-house-barbeque/menu?ref=google	https://1911bbq.com	BBQ	12	5
 3	Bamboo Grill Jamaican Restaurant	1005 Chambers St, Trenton, NJ 08611	Monday\t10AM - 7PM,\n        Tuesday\t10AM - 7PM,\n        Wednesday 10AM - 7PM,\n        Thursday 10AM - 7PM,\n        Friday 10AM - 8PM,\n        Saturday 10AM - 8PM,\n        Sunday Closed	t	None	None	Jamaican, Grill	1	3.2
-4	Ila Mae's Restaurant	313 Market St, Trenton, NJ 08611	Monday\tClosed,\n        Tuesday\t9AM - 8PM,\n        Wednesday 9AM - 8PM,\n        Thursday 9AM - 8PM,\n        Friday 9AM - 8PM,\n        Saturday 9AM - 8PM,\n        Sunday Closed	f	http://places.singleplatform.com/ila-maes-restaurant/menu?ref=google	None	Soul	10	4.3
+4	'Ila Mae's Restaurant'	313 Market St, Trenton, NJ 08611	Monday\tClosed,\n        Tuesday\t9AM - 8PM,\n        Wednesday 9AM - 8PM,\n        Thursday 9AM - 8PM,\n        Friday 9AM - 8PM,\n        Saturday 9AM - 8PM,\n        Sunday Closed	f	http://places.singleplatform.com/ila-maes-restaurant/menu?ref=google	None	Soul	10	4.3
 5	Blue Danube Restaurant	538 Adeline St, Trenton, NJ 08611	Monday\tClosed\n        Tuesday\t11:30AM - 2:15PM, 5 - 7:30PM,\n        Wednesday 11:30AM - 2:15PM, 5 - 7:30PM,\n        Thursday 11:30AM - 2:15PM, 5 - 7:30PM,\n        Friday 11:30AM - 2:15PM, 5 - 8PM,\n        Saturday 3 - 8PM,\n        Sunday 3 - 7:30PM	t	http://www.bluedanuberestaurant.net/menu.php	http://www.bluedanuberestaurant.net/about.php	Eastern European	273	4.6
 6	The Big Easy of Trenton Restaurant	111 S Warren St, Trenton, NJ 08608	Monday\t12 - 7PM,\n        Tuesday\t12 - 7PM,\n        Wednesday 12 - 7PM,\n        Thursday 12 - 7PM,\n        Friday 12 - 7PM,\n        Saturday 9AM - 5PM,\n        Sunday Closed	f	None	None	Dine-in	260	4.5
-7	Don Julio's Bar and Grill	900 Liberty St, Trenton, NJ 08611	Monday\t11AM - 2AM,\n        Tuesday\t11AM - 2AM,\n        Wednesday 11AM - 2AM,\n        Thursday 11AM - 2AM,\n        Friday 11AM - 2AM,\n        Saturday 11AM - 2AM,\n        Sunday 12PM - 2AM	t	None	None	Bar & Grill	190	4.3
+7	'Don Julio's Bar and Grill'	900 Liberty St, Trenton, NJ 08611	Monday\t11AM - 2AM,\n        Tuesday\t11AM - 2AM,\n        Wednesday 11AM - 2AM,\n        Thursday 11AM - 2AM,\n        Friday 11AM - 2AM,\n        Saturday 11AM - 2AM,\n        Sunday 12PM - 2AM	t	None	None	Bar & Grill	190	4.3
 8	The Hummingbird Restaurant	29 S Warren St, Trenton, NJ 08608	Monday\t10AM - 7PM,\n        Tuesday\t10AM - 7PM,\n        Wednesday 10AM - 7PM,\n        Thursday 10AM - 7PM,\n        Friday 10AM - 6PM,\n        Saturday 10AM - 6PM,\n        Sunday Closed	f	None	None	Jamaican	282	4.2
 9	Sabor Latino	293 Ashmore Ave, Trenton, NJ 08611	Tuesday 10AM - 12AM,\n        Wednesday Closed,\n        Thursday 10AM - 12AM,\n        Friday 10AM - 2AM,\n        Saturday 10AM - 2AM,\n        Sunday 10AM - 12AM,\n        Monday 10AM - 12AM	t	None	None	Dominican	80	4.1
-10	Trentini's	635 S Clinton Ave, Trenton, NJ 08611	Tuesday 10AM - 10PM,\n        Wednesday 10AM - 10PM,\n        Thursday 10AM - 10PM,\n        Friday 10AM - 10PM,\n        Saturday 10AM - 10PM,\n        Sunday 10AM - 10PM,\n        Monday 10AM - 10PM	f	https://trentinismenu.com/menu.html	https://trentinismenu.com/index.html	Italian	420	4.1
+10	'Trentini's'	635 S Clinton Ave, Trenton, NJ 08611	Tuesday 10AM - 10PM,\n        Wednesday 10AM - 10PM,\n        Thursday 10AM - 10PM,\n        Friday 10AM - 10PM,\n        Saturday 10AM - 10PM,\n        Sunday 10AM - 10PM,\n        Monday 10AM - 10PM	f	https://trentinismenu.com/menu.html	https://trentinismenu.com/index.html	Italian	420	4.1
 11	Mama D Soul Food 2	312 S Broad St, Trenton, NJ 08609	Tuesday Closed,\n        Wednesday 12 - 7PM,\n        Thursday 12 - 7PM,\n        Friday 12 - 7PM,\n        Saturday 9AM - 7PM,\n        Sunday 1 - 7PM,\n        Monday Closed	t	None	None	Soul	112	3.9
-12	Cooper's Riverview	50 Riverview Plaza, Trenton, NJ 08611	Tuesday 12PM - 2AM,\n        Wednesday 12PM - 2AM,\n        Thursday 12PM - 2AM,\n        Friday 12PM - 2AM,\n        Saturday 12PM - 2AM,\n        Sunday 11:30AM - 11PM,\n        Monday Closed	f	coopersnj.com	None	English	455	4
+12	'Cooper's Riverview'	50 Riverview Plaza, Trenton, NJ 08611	Tuesday 12PM - 2AM,\n        Wednesday 12PM - 2AM,\n        Thursday 12PM - 2AM,\n        Friday 12PM - 2AM,\n        Saturday 12PM - 2AM,\n        Sunday 11:30AM - 11PM,\n        Monday Closed	f	coopersnj.com	None	English	455	4
 13	Mi Ranchito Pizza and Tacos	911 Chambers St, Trenton, NJ 08611	Tuesday 10AM - 10PM,\n        Wednesday Closed,\n        Thursday 10AM - 10PM,\n        Friday 10AM - 10PM,\n        Saturday 10AM - 10PM,\n        Sunday 10AM - 10PM,\n        Monday 10AM - 10PM	t	None	None	Tacos	61	4.7
 14	El Potrillo	541 Roebling Ave, Trenton, NJ 08611	Tuesday Closed,\n        Wednesday 10AM - 10PM,\n        Thursday 10AM - 10PM,\n        Friday 10AM - 10PM,\n        Saturday 10AM - 10PM,\n        Sunday 10AM - 10PM,\n        Monday 10AM - 10PM	f	http://places.singleplatform.com/el-potrillo-mexican-restaurant-7/menu?ref=google	None	Mexican	213	3.9
 15	Chencha y Chole	865 S Broad St, Trenton, NJ 08611	11AM - 10PM everyday	t	None	None	Mexican	319	4.2
@@ -161,19 +137,11 @@ COPY public.reviews (review_id, customer_id, restaurant_id, date, text, price, t
 
 
 --
--- Data for Name: usertype; Type: TABLE DATA; Schema: public; Owner: rmd
---
-
-COPY public.usertype (user_id, customer_id, restaurant_id, admin, restaurant, customer) FROM stdin;
-\.
-
-
---
 -- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: rmd
 --
 
 ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT categories_pkey PRIMARY KEY (restaurant_id);
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (category);
 
 
 --
@@ -201,14 +169,6 @@ ALTER TABLE ONLY public.reviews
 
 
 --
--- Name: usertype usertype_pkey; Type: CONSTRAINT; Schema: public; Owner: rmd
---
-
-ALTER TABLE ONLY public.usertype
-    ADD CONSTRAINT usertype_pkey PRIMARY KEY (user_id);
-
-
---
 -- Name: categories categories_restaurant_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rmd
 --
 
@@ -230,22 +190,6 @@ ALTER TABLE ONLY public.reviews
 
 ALTER TABLE ONLY public.reviews
     ADD CONSTRAINT reviews_restaurant_id_fkey FOREIGN KEY (restaurant_id) REFERENCES public.restaurants(restaurant_id);
-
-
---
--- Name: usertype usertype_customer_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rmd
---
-
-ALTER TABLE ONLY public.usertype
-    ADD CONSTRAINT usertype_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES public.customers(customer_id);
-
-
---
--- Name: usertype usertype_restaurant_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rmd
---
-
-ALTER TABLE ONLY public.usertype
-    ADD CONSTRAINT usertype_restaurant_id_fkey FOREIGN KEY (restaurant_id) REFERENCES public.restaurants(restaurant_id);
 
 
 --
