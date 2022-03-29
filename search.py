b0ac85bcb5140d16a05c32ab63f5731eeb93b2ec
Justice Chukwuma <chukwuma@princeton.edu>
2022-03-28 20:48:32 -0400

    last commit

--- search.py
+++ search.py
@@ -191,6 +191,7 @@ def restaurant_search(input):
     try:
         # with connect(host='localhost', port=5432, user='rmd', password='xxx',
         #              database="trentoneats") as connection:
+        # dequ5ope4nuoit
         with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
                      database="dequ5ope4nuoit") as connection:
 
