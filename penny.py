bf507f799e427b8ab366270fe5ce17f7ddbe2d02
Piers Ozuah <pjozuah@princeton.edu>
2022-04-05 10:36:42 -0400

    Debugging login internal error

--- penny.py
+++ penny.py
@@ -169,7 +169,7 @@ def login_is_required(function):
 @app.route('/login', methods=['GET'])
 def login():
 
-    session.clear()
+    # session.clear()
     # CHECK IF LOGGED IN IF LOGGED IN THEN LOG OUT
     authorization_url, state = flow.authorization_url()
     print(authorization_url)
@@ -182,6 +182,8 @@ def login():
 
 @app.route('/callback', methods=['GET'])
 def callback():
+    # Debugging
+    print(session["state"])
     flow.fetch_token(authorization_response=request.url)
     print(request.url)
 
