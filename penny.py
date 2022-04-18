b79e91dce1e44f7d8f23500a62b7909fc8e2fa84
Piers Ozuah <pjozuah@princeton.edu>
2022-04-18 19:55:57 -0400

    Added requests button when admin is logged in

--- penny.py
+++ penny.py
@@ -13,7 +13,7 @@ from add_restaurant import add_restaurant
 
 from reviews import add_review, review_search
 
-from admin import admin_search 
+from admin import admin_search
 from users import user_exists, user_add, add_favorite_restaurant, is_favorite_restaurant, get_favorites
 
 import datetime
@@ -41,7 +41,9 @@ def search_form():
     if error_msg is None:
         error_msg = ''
     unique_id = session.get('google_id')
-    html = render_template('searchform.html', error_msg=error_msg, id=unique_id)
+    admin = is_admin()
+    html = render_template(
+        'searchform.html', error_msg=error_msg, id=unique_id, admin=admin)
     response = make_response(html)
     return response
 
@@ -55,8 +57,8 @@ def search_results():
         error_msg = ''
 
     restaurant = request.args.get('restaurant')
-    if (restaurant is None) or (restaurant.split()==""):
-        restaurant=""
+    if (restaurant is None) or (restaurant.split() == ""):
+        restaurant = ""
 
     try:
         restaurantinfo = restaurant_search(restaurant)
@@ -69,26 +71,28 @@ def search_results():
         response = make_response('<div><p>Standard Server Error</p></div>')
         return response
     unique_id = session.get('google_id')
+    admin = is_admin()
     html = render_template('searchresults.html', restaurantinfo=restaurantinfo,
-                            id=unique_id)
+                           id=unique_id, admin=admin)
     response = make_response(html)
-    
+
     return response
 # ---------------------------------------------------------
-
 
 
 @app.route('/myrestaurant', methods=['GET'])
 def myrestaurant():
     unique_id = session.get('google_id')
+    admin = is_admin()
     favorites = get_favorites(session["email"])
-    #for restaurant in favorites
+    # for restaurant in favorites
 
     resid = favorites[1]
     print("this is resid" + resid)
     restaurantinfo = restaurant_search(resid)
     #html = render_template('myrestaurant.html', id=unique_id)
-    html = render_template('myrestaurant.html', restaurantinfo = restaurantinfo)
+    html = render_template('myrestaurant.html',
+                           restaurantinfo=restaurantinfo, admin=admin)
     response = make_response(html)
     return response
 
@@ -99,11 +103,19 @@ def myrestaurant():
 @app.route('/about', methods=['GET'])
 def about():
     unique_id = session.get('google_id')
-    html = render_template('about.html', id=unique_id)
+    admin = is_admin()
+    html = render_template('about.html', id=unique_id, admin=admin)
     response = make_response(html)
     return response
 
 # ---------------------------------------------------------
+
+
+def is_admin():
+    if ((session.get("email") is None) or (not admin_search(session["email"]))):
+        return False
+    else:
+        return True
 
 
 def authorized(function):
@@ -122,8 +134,9 @@ def authorized(function):
 @app.route('/joinrestaurant', methods=['GET'])
 @authorized
 def joinrestaurant():
+    admin = is_admin()
     unique_id = session.get('google_id')
-    html = render_template('joinrestaurant.html', id=unique_id)
+    html = render_template('joinrestaurant.html', id=unique_id, admin=admin)
     response = make_response(html)
     return response
 
@@ -133,81 +146,87 @@ def joinrestaurant():
 @app.route('/addrestaurant', methods=['GET'])
 def addrestaurant():
     restaurantName = request.args.get('restaurantName')
-    if restaurantName is None or restaurantName.split()=="":
+    if restaurantName is None or restaurantName.split() == "":
         restaurantName = ""
     restaurantAddress = request.args.get('restaurantAddress')
-    if restaurantAddress is None or restaurantAddress.split()=="":
+    if restaurantAddress is None or restaurantAddress.split() == "":
         restaurantAddress = ""
     # RESTAURANT HOURS START
     restaurantHours = ""
 
-    # -> weekday lol 
+    # -> weekday lol
     weekdayHours = ""
     if request.args.get('mondayopen'):
-        weekdayHours += "Monday " + request.args.get('mondaystart') + " - " + request.args.get('mondayend') + ", "
+        weekdayHours += "Monday " + \
+            request.args.get('mondaystart') + " - " + \
+            request.args.get('mondayend') + ", "
     else:
         weekdayHours += "Monday Closed, "
     if request.args.get('tuesdayopen'):
-        weekdayHours += "Tuesday " + request.args.get('tuesdaystart') + " - " + request.args.get('tuesdayend') + ", "
+        weekdayHours += "Tuesday " + \
+            request.args.get('tuesdaystart') + " - " + \
+            request.args.get('tuesdayend') + ", "
     else:
         weekdayHours += "Tuesday Closed, "
     if request.args.get('wednesdayopen'):
-        weekdayHours += "Wednesday " + request.args.get('wednesdaystart') + " - " + request.args.get('wednesdayend') + ", "
+        weekdayHours += "Wednesday " + \
+            request.args.get('wednesdaystart') + " - " + \
+            request.args.get('wednesdayend') + ", "
     else:
         weekdayHours += "Wednesday Closed, "
     if request.args.get('thursdayopen'):
-        weekdayHours += "Thursday " + request.args.get('thursdaystart') + " - " + request.args.get('thursdayend') + ", "
+        weekdayHours += "Thursday " + \
+            request.args.get('thursdaystart') + " - " + \
+            request.args.get('thursdayend') + ", "
     else:
         weekdayHours += "Thursday Closed, "
     if request.args.get('fridayopen'):
-        weekdayHours += "Friday " + request.args.get('fridaystart') + " - " + request.args.get('fridayend') + ", "
+        weekdayHours += "Friday " + \
+            request.args.get('fridaystart') + " - " + \
+            request.args.get('fridayend') + ", "
     else:
         weekdayHours += "Friday Closed, "
     if request.args.get('saturdayopen'):
-        weekdayHours += "Saturday " + request.args.get('saturdaystart') + " - " + request.args.get('saturdayend') + ", "
+        weekdayHours += "Saturday " + \
+            request.args.get('saturdaystart') + " - " + \
+            request.args.get('saturdayend') + ", "
     else:
         weekdayHours += "Saturday Closed, "
     if request.args.get('sundayopen'):
-        weekdayHours += "Sunday " + request.args.get('sundaystart') + " - " + request.args.get('sundayend')
+        weekdayHours += "Sunday " + \
+            request.args.get('sundaystart') + " - " + \
+            request.args.get('sundayend')
     else:
         weekdayHours += "Sunday Closed"
-    
-    
-    
-    
-    
-    
-
 
     restaurantHours = weekdayHours
 
-
     # change?
-    if restaurantHours is None or restaurantHours.split()=="":
+    if restaurantHours is None or restaurantHours.split() == "":
         restaurantHours = ""
     restaurantMenu = request.args.get('restaurantMenu')
-    if restaurantMenu is None or restaurantMenu.split()=="":
+    if restaurantMenu is None or restaurantMenu.split() == "":
         restaurantMenu = ""
     restaurantMedia = request.args.get('restaurantMedia')
-    if restaurantMedia is None or restaurantMedia.split()=="":
+    if restaurantMedia is None or restaurantMedia.split() == "":
         restaurantMedia = ""
     restaurantTags = request.args.get('restaurantTags')
     restaurantImage = request.args.get('restaurantImage')
 
-    if restaurantTags is None or restaurantTags.split()=="":
+    if restaurantTags is None or restaurantTags.split() == "":
         restaurantTags = ""
 
-    type= request.args.getlist('type')
+    type = request.args.getlist('type')
     for i in range(len(type)):
-        if type[i] is None or type[i].split()=="":
+        if type[i] is None or type[i].split() == "":
             type[i] = ""
     cuisine = request.args.getlist('cuisine')
     for i in range(len(cuisine)):
-        if cuisine[i] is None or cuisine[i].split()=="":
+        if cuisine[i] is None or cuisine[i].split() == "":
             cuisine[i] = ""
 
     priceNum = request.args.get('price')
-    if priceNum is None or priceNum.split()=="":
+    if priceNum is None or priceNum.split() == "":
         priceNum = ""
     priceNum = int(priceNum)
     price = 'inexpensive'
@@ -216,32 +235,34 @@ def addrestaurant():
     if priceNum >= 25:
         price = 'pricey'
 
-
-    add_restaurant(restaurantName = restaurantName,
-        restaurantAddress = restaurantAddress,
-        restaurantHours = restaurantHours,
-        restaurantMenu = restaurantMenu,
-        restaurantMedia = restaurantMedia,
-        restaurantTags = restaurantTags,
-        cuisine = cuisine,
-        type = type,
-        price = price,
-        restaurantImage = restaurantImage)
+    add_restaurant(restaurantName=restaurantName,
+                   restaurantAddress=restaurantAddress,
+                   restaurantHours=restaurantHours,
+                   restaurantMenu=restaurantMenu,
+                   restaurantMedia=restaurantMedia,
+                   restaurantTags=restaurantTags,
+                   cuisine=cuisine,
+                   type=type,
+                   price=price,
+                   restaurantImage=restaurantImage)
     unique_id = session.get('google_id')
-    html = render_template('joinrestaurant.html', id=unique_id)
+    admin = is_admin()
+    html = render_template('joinrestaurant.html', id=unique_id, admin=admin)
     response = make_response(html)
     return response
 
 # ---------------------------------------------------------
 
+
 @app.route('/resdetails', methods=['GET', 'POST'])
 def resdetails():
     name = request.args.get('name')
     id = request.args.get('id')
+    admin = is_admin()
     session['resid'] = id
-    # Info currently is: 
+    # Info currently is:
     info = get_restaurant_info(id)
-    # This line is new. 
+    # This line is new.
     unique_id = session.get('google_id')
     #html = render_template('resdetails.html', info=info, id=unique_id)
 
@@ -257,12 +278,12 @@ def resdetails():
     # info_obj['stars'] = str(row[8])
     # info_obj['image'] = str(row[9])
 
-    #Separate Code here 
-    #Stmt
-    #reviews = db.execute(
+    # Separate Code here
+    # Stmt
+    # reviews = db.execute(
     # Just gotta fetch one of them. current_user_review = db.execute("SELECT * FROM reviews WHERE book_id=:book_id AND user_id=:user_id", {"book_id": book.id, "user_id": session["user_id"]}).fetchone()
     #book_extra = goodreads_lookup(isbn)
-    
+
     if request.method == 'POST':
         text = request.form['review-text']
         rating = request.form['rating']
@@ -274,35 +295,35 @@ def resdetails():
         if error is None:
             # Gotta figure out if its customer taht we still want to link for placeolder
             # Currently, placeholder reviews are all -10
-            #NEed Cusomter Ids sorted out.
+            # NEed Cusomter Ids sorted out.
             add_review(id, datetime.datetime.now(), text, rating)
-            #return redirect(url_for('review.dashboard'))
+            # return redirect(url_for('review.dashboard'))
             reviews = review_search(id)
-        #flash(error)
-        #return render_template('review/create.html')
-
-    
+        # flash(error)
+        # return render_template('review/create.html')
 
     html = render_template('resdetails.html', info=info,
-                                            reviews=reviews, id=unique_id)
+                           reviews=reviews, id=unique_id, admin=admin)
     response = make_response(html)
     return response
+
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     #name = request.args.get('name')
     #id = request.args.get('id')
     name = request.args.get('name')
-    id = session['resid'] 
+    id = session['resid']
     info = get_restaurant_info(id)
-    # This line is new. 
+    # This line is new.
     unique_id = session.get('google_id')
+    admin = is_admin()
     #html = render_template('resdetails.html', info=info, id=unique_id)
 
     reviews = review_search(id)
-    # Info currently is: 
+    # Info currently is:
     info = get_restaurant_info(id)
-    # This line is new. 
+    # This line is new.
     #html = render_template('resdetails.html', info=info, id=unique_id)
 
     # info_obj['name'] = str(row[0])
@@ -316,13 +337,13 @@ def test():
     # info_obj['stars'] = str(row[8])
     # info_obj['image'] = str(row[9])
 
-    # -- ADD TO FAVORITE -- 
+    # -- ADD TO FAVORITE --
     if (not is_favorite_restaurant(session["email"], id)):
         add_favorite_restaurant(session["email"], id)
     else:
         print("already a favorite!")
         # HOW DO I MAKE THIS DISSAPEAR.
-    
+
     if request.method == 'POST':
         text = request.form['review-text']
         rating = request.form['rating']
@@ -334,37 +355,33 @@ def test():
         if error is None:
             # Gotta figure out if its customer taht we still want to link for placeolder
             # Currently, placeholder reviews are all -10
-            #NEed Cusomter Ids sorted out.
+            # NEed Cusomter Ids sorted out.
             add_review(id, datetime.datetime.now(), text, rating)
-            #return redirect(url_for('review.dashboard'))
+            # return redirect(url_for('review.dashboard'))
             reviews = review_search(id)
-        #flash(error)
-        #return render_template('review/create.html')
-
-    
+        # flash(error)
+        # return render_template('review/create.html')
 
     html = render_template('resdetails.html', info=info,
-                                            reviews=reviews, id=unique_id)
+                           reviews=reviews, id=unique_id, admin=admin)
     response = make_response(html)
     return response
 
-    
-
     html = render_template('resdetails.html', info=info,
-                                            reviews=reviews, id=unique_id)
+                           reviews=reviews, id=unique_id)
     response = make_response(html)
     return response
     id = session['resid']
     return redirect('/resdetails', id=id)
-    # Info currently is: 
+    # Info currently is:
     info = get_restaurant_info(id)
-    # This line is new. 
+    # This line is new.
     unique_id = session.get('google_id')
     #html = render_template('resdetails.html', info=info, id=unique_id)
     reviews = review_search(id)
 
     html = render_template('resdetails.html', info=info,
-                                            reviews=reviews, id=unique_id)
+                           reviews=reviews, id=unique_id)
     response = make_response(html)
     return responsez
 # ---------------------------------------------------------
@@ -381,8 +398,8 @@ flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="https://trentoneats.herokuapp.com/callback"
-    #redirect_uri="http://127.0.0.1:8080/callback"
+    # redirect_uri="https://trentoneats.herokuapp.com/callback"
+    redirect_uri="http://127.0.0.1:8080/callback"
 )
 
 
@@ -442,14 +459,15 @@ def callback():
         user_add(session["email"], session["name"])
 
     unique_id = session.get('google_id')
-    #restaurant = ""
-    #restaurantinfo = restaurant_search(
-     #   restaurant)  # Exception handling omitted
+    admin = is_admin()
+    # restaurant = ""
+    # restaurantinfo = restaurant_search(
+    #     restaurant)  # Exception handling omitted
     html = render_template('searchform.html',
                            #ampm = get_ampm(),
                            # current_time = get_current_time(),
-                           #restaurantinfo=restaurantinfo,
-                           id=unique_id)
+                           #    restaurantinfo=restaurantinfo,
+                           id=unique_id, admin=admin)
     response = make_response(html)
     return response
     # return redirect("/")  # ,id=unique_id)
@@ -475,3 +493,11 @@ def authenticate():
         abort(redirect(url_for('login')))
     return session.get('name')
 
+
+@app.route('/requests', methods=['GET'])
+def reqeusts_area():
+    unique_id = session.get('google_id')
+    admin = is_admin()
+    html = render_template('about.html', id=unique_id, admin=admin)
+    response = make_response(html)
+    return response
