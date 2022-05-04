50ed7b13cdda1ff0fe21d38311e556bb22fec42e
Justice Chukwuma <chukwuma@princeton.edu>
2022-05-03 23:46:45 -0400

    fix: made all connections to heroku

--- add_user.py
+++ add_user.py
@@ -18,7 +18,7 @@ def add_customer(unique_id, name):
     id = str(unique_id)
     name = str(name)
     stmt_str = """
-    INSERT INTO customers (customer_id, name, review_count , avg_rating, 
+    INSERT INTO customers (customer_id, name, review_count , avg_rating,
     account_type, reported_count)
     SELECT """
     stmt_str += id + ', ' + "'" + name + "'" + ', 0, 0, 0, 0'
@@ -26,11 +26,11 @@ def add_customer(unique_id, name):
     SELECT 1 FROM customers WHERE customer_id= """ + "(CAST('" + id + "' AS VARCHAR(255))))"
 
     try:
-        with connect(
-                host='localhost', port=5432, user='rmd', password='trentoneats333',
-                database='trentoneats') as connection:
-            # with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
-            #              database="dequ5ope4nuoit") as connection:
+        # with connect(
+        #         host='localhost', port=5432, user='rmd', password='trentoneats333',
+        #         database='trentoneats') as connection:
+        with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
+                     database="dequ5ope4nuoit") as connection:
 
             with connection.cursor() as cursor:
                 print(stmt_str)
@@ -51,11 +51,11 @@ def add_user(unique_id):
     SELECT 1 FROM usertype WHERE user_id= """ + "(CAST('" + id + "' AS VARCHAR(255))))"
 
     try:
-        with connect(
-                host='localhost', port=5432, user='rmd', password='trentoneats333',
-                database='trentoneats') as connection:
-            # with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
-            #              database="dequ5ope4nuoit") as connection:
+        # with connect(
+        #         host='localhost', port=5432, user='rmd', password='trentoneats333',
+        #         database='trentoneats') as connection:
+        with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
+                     database="dequ5ope4nuoit") as connection:
 
             with connection.cursor() as cursor:
                 print(stmt_str)
@@ -75,11 +75,11 @@ def old_user(unique_id):
     """
     stmt_str += "(CAST('" + id + "' AS VARCHAR(255)))) THEN TRUE ELSE FALSE END"
     try:
-        with connect(
-                host='localhost', port=5432, user='rmd', password='trentoneats333',
-                database='trentoneats') as connection:
-            # with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
-            #              database="dequ5ope4nuoit") as connection:
+        # with connect(
+        #         host='localhost', port=5432, user='rmd', password='trentoneats333',
+        #         database='trentoneats') as connection:
+        with connect(host='ec2-3-229-161-70.compute-1.amazonaws.com', port=5432, user='jazlvqafdamomp', password='6bc2f9e25e0ab4a2e167d5aed92096137eaacd1667e2863a6659e019dbb7e81a',
+                     database="dequ5ope4nuoit") as connection:
 
             with connection.cursor() as cursor:
                 print(stmt_str)
